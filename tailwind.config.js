8326a5508a8e3d14e8b0d7cdb61c7ff9eed9e657
Rifannvl <riffanafendi@gmail.com>
2024-11-17 00:27:16 +0700

    menambahkan dependensi pada tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "grid-pattern":
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.1) 1px, transparent 1px), linear-gradient(90deg, rgba(0, 0, 0, 0.1) 1px, transparent 1px) border-box, linear-gradient(0deg, rgba(0, 0, 0, 0.1) 1px, transparent 1px) border-box",
+      },
+      backgroundSize: {
+        grid: "10px 10px", // Ukuran kotak dalam grid
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
